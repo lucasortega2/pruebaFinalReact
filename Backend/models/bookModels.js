8a17d51642b483cd2941607d523815237b5163d9
Lucasortega2 <lucas.ortega1999@gmail.com>
2024-03-26 11:45:27 -0300

    manera de importar el json con require

--- Backend/models/bookModels.js
+++ Backend/models/bookModels.js
@@ -1,44 +1,49 @@
-import books from "../books.json" with {type : "json"}
-import { randomUUID } from 'node:crypto'
-import fs from "fs"
+import { randomUUID } from 'node:crypto';
+import fs from 'fs';
 export const getAllBooksModel = () => {
-    return books
-}
+  return books;
+};
+import { createRequire } from 'node:module';
 
-export const getBookForIdModel = ({id}) => {
-    const book = books.filter((book) => book.id === id)
-    return book
+const require = createRequire(import.meta.url);
+const books = require('../books.json');
+export const getBookForIdModel = ({ id }) => {
+  const book = books.filter((book) => book.id === id);
+  return book;
 };
 export const createBookModel = ({ input }) => {
-    const newBook = {
-        id: input.id ? input.id : randomUUID() ,
-        ...input
-    }
-    const newBooks = [...books, newBook]
-    fs.writeFileSync("books.json", JSON.stringify(newBooks,null,2),"utf-8");
-    return newBook
-}
+  const newBook = {
+    id: input.id ? input.id : randomUUID(),
+    ...input,
+  };
+  const newBooks = [...books, newBook];
+  fs.writeFileSync('books.json', JSON.stringify(newBooks, null, 2), 'utf-8');
+  return newBook;
+};
 
 export const updateBookModel = ({ id, data }) => {
+  const updatedBooks = books.map((book) => {
+    if (book.id === id) {
+      return {
+        ...book,
+        ...data,
+      };
+    }
+    return book;
+  });
 
-     const updatedBooks = books.map(book => {
-        if (book.id === id) {
-            return {
-                ...book,
-                ...data
-            };
-        }
-        return book; 
-    });
-    
- fs.writeFileSync("books.json", JSON.stringify(updatedBooks,null,2),"utf-8");
-    return data
-}
+  fs.writeFileSync(
+    'books.json',
+    JSON.stringify(updatedBooks, null, 2),
+    'utf-8',
+  );
+  return data;
+};
 
-export const deleteBookModel = ({id}) => {
-    const bookIndex = books.findIndex(book => book.id === id)
-    console.log(bookIndex);
-    books.splice(bookIndex, 1)
-    fs.writeFileSync("books.json", JSON.stringify(books,null,2),"utf-8");
-    return true
-}
+export const deleteBookModel = ({ id }) => {
+  const bookIndex = books.findIndex((book) => book.id === id);
+  console.log(bookIndex);
+  books.splice(bookIndex, 1);
+  fs.writeFileSync('books.json', JSON.stringify(books, null, 2), 'utf-8');
+  return true;
+};
